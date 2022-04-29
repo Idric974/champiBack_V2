34510dec5a879ee22f4d11d55c117cc04fa7520d
Idric Evarne <idric.evarne@gmail.com>
2022-04-29 19:56:52 +0200

    Code pour tester la nouvelle master

--- gestion/gestionCo2/gestionCo2Test.js
+++ gestion/gestionCo2/gestionCo2Test.js
@@ -1,12 +1,13 @@
+//! Solution 1.
+
 const axios = require('axios');
 
 axios({
   method: 'post',
-  url: `http://localhost:5000/api/getCo2Routes/getCo2`,
-  //   url: `http://192.168.0.10:6000/getCO2/2`,
+  url: `http://192.168.0.10:5000/api/getCo2Routes/getCo2`,
   withCredentials: true,
   data: {
-    numSalle: '2',
+    numSalle: '1',
   },
 })
   .then((res) => {
@@ -15,3 +16,41 @@ axios({
   .catch((err) => {
     console.log(err);
   });
+
+//! -------------------------------------------------
+
+//! Solution 2.
+
+// const http = require('http');
+
+// const data = JSON.stringify({
+//   numSalle: 1,
+// });
+
+// const options = {
+//   hostname: '192.168.0.10',
+//   port: 5000,
+//   path: '/api/getCo2Routes/getCo2',
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     'Content-Length': data.length,
+//   },
+// };
+
+// const req = http.request(options, (res) => {
+//   console.log(`statusCode : ${res.statusCode}`);
+
+//   res.on('data', (d) => {
+//     process.stdout.write(d);
+//   });
+// });
+
+// req.on('error', (error) => {
+//   console.error(error);
+// });
+
+// req.write(data);
+// req.end();
+
+//! -------------------------------------------------
