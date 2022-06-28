d015a7e4a923aaed419d7be208c16ebbce7d0fed
Idric Evarne <idric.evarne@gmail.com>
2022-06-28 15:44:18 +0200

    Mise à jour CronTab + test master 1.1

--- gestion/gestionCo2/gestionCo2.js
+++ gestion/gestionCo2/gestionCo2.js
@@ -34,8 +34,8 @@ let valeurAxeX;
 
 //! Demande de mesure à la master.
 
-const url = 'http://localhost:5000/getCo2/' + numSalle;
-// const url = 'http://192.168.0.10:5000/getCO2/' + numSalle;
+// const url = 'http://localhost:5000/getCo2/' + numSalle;
+const url = 'http://192.168.0.10:5000/getCO2/' + numSalle;
 // console.log('url : ', url);
 
 const getTauxCo2 = new Promise((resolve, reject) => {
