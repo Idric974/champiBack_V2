22e54fab9cb8559f91643c1327d0f7f4c8919940
Idric Evarne <68675330+Idric974@users.noreply.github.com>
2024-06-01 19:06:56 +0400

    Delete routes directory

--- routes/logsBackRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const affichageLogsBack = require('../controllers/logsControllers');
-router.get('/getLogsBack', affichageLogsBack.getLogsBack);
-
-module.exports = router;
